c7a3ae0182ea39d77fcb92ef39a0c3d6fc47ee6a
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-14 23:00:10 +0530

    4. calling the function for one time🕛😀

--- js/clock.js
+++ js/clock.js
@@ -5,11 +5,12 @@ function clock() {
     const hours = time.getHours();
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
-    
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
 }
 
 
-
+clock();
 
 
 
